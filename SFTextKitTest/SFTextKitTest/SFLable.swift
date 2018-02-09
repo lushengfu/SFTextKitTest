785964e8af44f3ec5f104e45cdd7d1a955a3fd27
lushengfu <cylushengfu@163.com>
2018-02-09 18:38:38 +0800

    初始化项目

---
+++ SFTextKitTest/SFTextKitTest/SFLable.swift
@@ -0,0 +1,14 @@
+//
+//  SFLable.swift
+//  SFTextKitTest
+//
+//  Created by happy on 2018/2/9.
+//  Copyright © 2018年 happy. All rights reserved.
+//
+
+import UIKit
+
+class SFLable: UILabel {
+
+
+}
---
+++ SFTextKitTest/SFTextKitTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SFTextKitTest
+//
+//  Created by happy on 2018/2/9.
+//  Copyright © 2018年 happy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lable: SFLable!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+}
+
